fb00dfe8e3ec7204bba4c3177191e864453751ef
Syed <syeds.devops007@gmail.com>
2024-05-30 00:24:25 +0530

    feat(git_util): add genrateReposUrl and AddPagination.

--- pkg/git/git_utils.go
+++ pkg/git/git_utils.go
@@ -1,14 +1,54 @@
 package git
 
 import (
+	"fmt"
 	"strings"
 
 	"github.com/SyedDevop/gitpuller/pkg/assert"
 	gituser "github.com/SyedDevop/gitpuller/pkg/git/git-user"
 )
 
-// Response:  <https://api.github.com/user/89797705/repos?per_page=20&page=2>; rel="next", <https://api.github.com/user/89797705/repos?per_page=20&page=2>; rel="last"
+// GenerateReposURL generates a GitHub API URL to fetch repositories for a given user.
+// It takes a user's name as input, trims any leading or trailing whitespace, and
+// returns the formatted URL as a string.
+//
+// Parameters:
+//   - name: The GitHub username as a string.
+//
+// Returns:
+//   - A formatted URL string to access the user's repositories on GitHub.
+func GenerateReposURL(name string) string {
+	return fmt.Sprintf("https://api.github.com/user/%s/repos", strings.TrimSpace(name))
+}
+
+// AddPaginationParams adds pagination parameters to a given URL.
+//
+// It takes a base URL and pointers to the number of items per page and the number of pages.
+// If the provided per or pages parameters are nil, default values of 50 and 1 are used respectively.
+//
+// Parameters:
+//   - url: The base URL as a string.
+//   - per: A pointer to an integer specifying the number of items per page.
+//   - pages: A pointer to an integer specifying the number of pages.
+//
+// Returns:
+//   - A formatted URL string with the pagination parameters appended.
+func AddPaginationParams(url string, per, pages *int) string {
+	if per == nil {
+		defaultPer := 50
+		per = &defaultPer
+	}
+
+	if pages == nil {
+		defaultPages := 1
+		pages = &defaultPages
+	}
+
+	return fmt.Sprintf("%s?per_page=%d&pages=%d", url, *per, *pages)
+}
+
 func ParseLinkHeader(rawLink string) []*gituser.Link {
+	// Response:  <https://api.github.com/user/89797705/repos?per_page=20&page=2>; rel="next", <https://api.github.com/user/89797705/repos?per_page=20&page=2>; rel="last"
 	if len(rawLink) < 2 {
 		return nil
 	}
