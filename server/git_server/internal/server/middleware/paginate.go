b7bac6ec29cbdf5de71bd7c74395c50213f38de2
Syed <syeds.devops007@gmail.com>
2024-06-05 01:30:18 +0530

    feat(server): add paginatio middleware

---
+++ server/git_server/internal/server/middleware/paginate.go
@@ -0,0 +1,50 @@
+package middleware
+
+import (
+	"context"
+	"net/http"
+	"strconv"
+)
+
+type (
+	PerPage struct {
+		name string
+	}
+	Page struct {
+		name string
+	}
+)
+
+func (p *PerPage) String() string {
+	return p.name
+}
+
+func (p *Page) String() string {
+	return p.name
+}
+
+var (
+	PerPageKey = &PerPage{"per_page"}
+	PageKey    = &Page{"page"}
+)
+
+func Paginate(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		perPageStr := r.URL.Query().Get(PerPageKey.String())
+		pageStr := r.URL.Query().Get(PageKey.String())
+
+		perPage, err := strconv.Atoi(perPageStr)
+		if err != nil || perPage < 1 {
+			perPage = 30
+		}
+		page, err := strconv.Atoi(pageStr)
+		if err != nil || page < 1 {
+			page = 1
+		}
+		ctx := r.Context()
+		ctx = context.WithValue(ctx, PerPageKey, perPage)
+		ctx = context.WithValue(ctx, PageKey, page)
+		r = r.WithContext(ctx)
+		next.ServeHTTP(w, r)
+	})
+}
