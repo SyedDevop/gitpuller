782e57931c7b71251ad338c242faaf71417f4c74
Syed <syeds.devops007@gmail.com>
2024-06-17 00:47:43 +0530

    fix(util_test): the position of expected val
    - add 2 more test

--- cmd/util/util_test.go
+++ cmd/util/util_test.go
@@ -32,12 +32,12 @@ func TestGetParentPath(t *testing.T) {
 		isParent, parentPath := GetParentPath(workingPath)
 		workingPath = parentPath
 		if i >= 4 {
-			assert.Equal(t, parentPath, pathSteps1[2], "The path should be a parent from :last")
-			assert.Equal(t, isParent, true, "Is parent should be true from :last")
+			assert.Equal(t, pathSteps1[2], parentPath, "The path should be a parent from :last")
+			assert.Equal(t, true, isParent, "Is parent should be true from :last")
 			continue
 		}
-		assert.Equal(t, parentPath, pathSteps1[i-1], "The path should be a parent :else")
-		assert.Equal(t, isParent, false, "Is parent should be False :else")
+		assert.Equal(t, pathSteps1[i-1], parentPath, "The path should be a parent :else")
+		assert.Equal(t, false, isParent, "Is parent should be False :else")
 	}
 
 	workingPath2 := pathsList[1]
@@ -45,23 +45,31 @@ func TestGetParentPath(t *testing.T) {
 		isParent, parentPath := GetParentPath(workingPath2)
 		workingPath2 = parentPath
 		if i >= 4 {
-			assert.Equal(t, parentPath, pathSteps2[2], "The path should be a parent")
-			assert.Equal(t, isParent, true, "Is parent should be true")
+			assert.Equal(t, pathSteps2[2], parentPath, "The path should be a parent")
+			assert.Equal(t, true, isParent, "Is parent should be true")
 			continue
 		}
-		assert.Equal(t, parentPath, pathSteps2[i-1], "The path should be a parent")
-		assert.Equal(t, isParent, false, "Is parent should be False")
+		assert.Equal(t, pathSteps2[i-1], parentPath, "The path should be a parent")
+		assert.Equal(t, false, isParent, "Is parent should be False")
 	}
 
 	isParent, parentPath := GetParentPath(pathsList[2])
-	assert.Equal(t, parentPath, "/home", "The parent should be /home")
-	assert.Equal(t, isParent, true, "Is parent should be true for /home")
+	assert.Equal(t, "/home", parentPath, "The parent should be /home")
+	assert.Equal(t, true, isParent, "Is parent should be true for /home")
 
 	isParent1, parentPath1 := GetParentPath(pathsList[3])
-	assert.Equal(t, parentPath1, "Home", "The path should be a parent")
-	assert.Equal(t, isParent1, true, "Is parent should be False")
+	assert.Equal(t, "Home", parentPath1, "The path should be a parent")
+	assert.Equal(t, true, isParent1, "Is parent should be False")
 
 	isRoot, parentPath2 := GetParentPath("/")
-	assert.Equal(t, parentPath2, "/", "Passed '/' and should get '/'")
-	assert.Equal(t, isRoot, true, "This should be true")
+	assert.Equal(t, "/", parentPath2, "Passed '/' and should get '/'")
+	assert.Equal(t, true, isRoot, "This should be true")
+
+	isRoot, file := GetParentPath("test.json")
+	assert.Equal(t, "test.json", file, "Passed 'test.json' and should get 'test.json'")
+	assert.Equal(t, true, isRoot, "This should be true")
+
+	isRoot, file = GetParentPath("cmd")
+	assert.Equal(t, "cmd", file, "Passed 'cmd' and should get 'cmd'")
+	assert.Equal(t, true, isRoot, "This should be true")
 }
